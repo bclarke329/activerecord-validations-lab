65e56dee34f39f8d7611d7e6e2f5691d8a3e757d
bclarke329 <bmendoza329@hotmail.com>
2020-12-08 13:48:03 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 225}
+    validates :summary, length: { maximum: 225}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%w{category} is not a vaid category"}
+    validate :is_clickbait?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+
+    def is_clickbait?
+        if CLICKBAIT.none? {|phrases| phrases.match title }
+            errors.add(:title, "must be clickbait")
+        end 
+    end 
 end
